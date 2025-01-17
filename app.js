7044171c14e77c7028e4a51fbedcd3784819b1eb
Illian-Santiago <illianclase12345678@gmail.com>
2025-01-17 02:37:05 +0000

    App creada como en el ejercicio anterior

---
+++ app.js
@@ -0,0 +1,42 @@
+// 1. Instancias
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var favicon = require('serve-favicon');
+var path = require('path');
+var app = express();
+
+// 2. Configuraciones
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// 3. Middleware
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// 4. Rutas
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// 5. Manejadores de errores
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// 6. Exportación del servidor
+module.exports = app;
